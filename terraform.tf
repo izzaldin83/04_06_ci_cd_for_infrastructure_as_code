a82451137bf9ca2d32345404302e70730bc546e7
izzaldin83 <146792069+izzaldin83@users.noreply.github.com>
2023-11-09 09:10:34 +0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "elasticbeanstalk-ap-southeast-1-437725568050"             # the bucket
+    region = " ap-southeast-1"             # the region
   }
 }
